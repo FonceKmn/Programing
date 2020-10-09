c4a33f78c7f16c022645f294bd80900f7e36e940
KamranPc <jkrkmn@gmail.com>
2020-10-09 14:21:31 +0400

    dw
    dw

--- Node.Js/intro.js
+++ Node.Js/intro.js
@@ -1,22 +1,23 @@
-var http = require('http');
-var fs = require('fs');
+
+var http = require('http'); //http portunu bildirmek ucundu
+var fs = require('fs');   // filesystem modulu
 
 
-var server = http.createServer(function(req,res){
+var server = http.createServer(function(req,res){    // serverin yaradilmasi veisdifadeciden gelen request ve response ile ilgili
 
-     if(req.url == '/'){
-        fs.readFile('index.html',function(err,data){
+     if(req.url == '/'){    // req.url ile istifadeciden gelen get requestin sonlarina dogru
+        fs.readFile('index.html',function(err,data){  // request olaraqdan htmli oxuyur ve respons edir
 
-            res.write(data);
-            res.end('mesaj qutardi');
+            res.write(data);            // isdifadeciye gonderilen
+            res.end('mesaj qutardi ?? ');   // sehife sonlugu olaraq consoleden gorunur
         });
 
      }
 
-     if(req.url == '/sehife'){
+     if(req.url == '/sehife'){    // sehife olan page url sonlugu olaraq
         fs.readFile('index2.html',function(err,data){
 
-            res.write(data);
+            res.write(data);            // bunun burda yazilmagi node.js in random readininge qarsi onlemdir
             res.end('mesaj qutardi');
         });
 
@@ -31,4 +32,4 @@ var server = http.createServer(function(req,res){
    // res.end('mesaj qutardi');
 });
 
-server.listen(8000);
+server.listen(8000); // serverin qulaq asdigi port nomresi
