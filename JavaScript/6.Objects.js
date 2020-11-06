f61f15bed6c6e97aa9b644e072cadadfe73328bd
KamranPc <jkrkmn@gmail.com>
2020-11-06 20:03:32 +0400

    esas

--- JavaScript/6.Objects.js
+++ JavaScript/6.Objects.js
@@ -4,3 +4,31 @@ model : 'Corsa',
 yil : 2015,
 otumatik : true,
 }
+
+var toyota = new Object();
+
+toyota.marka = 'Toyota';
+toyota.il = 32;
+
+
+var mashinlar = [
+
+    {
+        marka : 'opel',
+        model : 'Corsa',
+        yil : 2015,
+        otumatik : true,
+        }
+
+  {
+        marka : 'opel',
+         model : 'Corsa',
+         yil : 2015,
+         otumatik : true,
+        }
+
+
+
+
+
+]
